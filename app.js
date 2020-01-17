9e0af83940b796cc3ff8463a4666f8b1d6f1462e
Ken Irwin <irwinkr@miamioh.edu>
2020-01-17 10:38:00 -0500

    removed unneed packages

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
 
 // Get the API Token
-const { oauth2, getToken } = require('./config/auth');
+const { oauth2, getToken, software  } = require('./config/auth');
 
 // With Token, make API calls
 getToken().then(values => {
   const token = values.access_token;
 
-  libcalQueries = ['bookings', 'locations', 'overdue'];
-  promises = [];
+  libcalQueries = ['bookings', 'locations', 'overdue', 'categories'];
+  var promises = [];
 
   libcalQueries.forEach(element => {
+    if (element == 'categories') { var id = '/8370'} else { id = ''}
     promises[element] = new Promise((resolve, reject) => {
-      var query = require('./config/apiQuery')({ path: '/1.1/equipment/' + element, token: token }).then((result) => {
+      var query = require('./config/apiQuery')({ path: '/1.1/equipment/' + element + id, token: token }).then((result) => {
         fs.writeFile(element + '.log', result, (err) => {
           console.log('Wrote new data to ' + element + '.log');
+          resolve(result);
         });
       })
         .catch('Failed to retrieve ' + element)
-        .finally(resolve(element + ' done'));
     })
   });
 
-  Promise.all([promises['bookings'], promises['locations'], promises['overdue']]).then((values) => {
-    console.log('all the promises: ', values);
+  Promise.all([ promises['bookings'], promises['locations'], promises['overdue'], promises['categories'] ]).then((values) => {
+    // const cats = JSON.parse(values[libcalQueries.indexOf('categories')])[0].categories;
+
   })
 })
 
