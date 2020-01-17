7360f47405d990eefdd4dca695920d811bf486e6
Ken Irwin <irwinkr@miamioh.edu>
2020-01-17 09:10:11 -0500

    auth in config, simple app file

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-//const token = require('./config/auth');
-//console.log('Recieved token: ', token);
-
-const libCal = require('./config/libCal');
-const oauth2 = require('simple-oauth2').create(libCal.credentials);
-
-function getToken() {
-  return new Promise((resolve, revoke) => {
-    async function getLibCalToken() {
-      try {
-        const result = await oauth2.clientCredentials.getToken();
-        const accessToken = oauth2.accessToken.create(result);
-        //console.log('Token: ', result);
-        resolve(result);
-
-      } catch (err) {
-        console.error('Access Token Error', err.message);
-      }
-    }
-    getLibCalToken();
-  });
-}
+const {oauth2, getToken} = require('./config/auth');
 
 getToken().then(values => {
-  console.log('got token:')
-  console.log(values)
+  const token = values.access_token;
+
 })
 
 
