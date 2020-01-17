9ea3c64bc48ee0ca3c612b2253b8f1b83abf4912
Ken Irwin <irwinkr@miamioh.edu>
2020-01-17 09:10:11 -0500

    auth in config, simple app file

--- config/auth.js
+++ config/auth.js
@@ -1,73 +1,22 @@
-const https = require('https');
-const querystring = require('querystring');
+const libCal = require('./libCal');
+const oauth2 = require('simple-oauth2').create(libCal.credentials);
 
-const postData = querystring.stringify({
-  client_id: '527',
-  client_secret: 'b566c23fdf8afc947a4426394b0ffb02',
-  grant_type: 'client_credentials'
-})
+module.exports = {
+  oauth2: oauth2,
+  getToken: function () {
+    return new Promise((resolve, revoke) => {
+      async function getLibCalToken() {
+        try {
+          const result = await oauth2.clientCredentials.getToken();
+          const accessToken = oauth2.accessToken.create(result);
+          //console.log('Token: ', result);
+          resolve(result);
 
-const options = {
-  hostname: 'muohio.libcal.com',
-  port: 443,
-  path: '/1.1/oauth/token',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': postData.length,
-    clientID: '527',
-    clientSecret: 'b566c23fdf8afc947a4426394b0ffb02',
-    grant_type: 'client_credentials'
+        } catch (err) {
+          console.error('Access Token Error', err.message);
+        }
+      }
+      getLibCalToken();
+    });
   }
 }
-
-const getToken = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // console.log(options.hostname);
-    }, 1000);
-    
-    resolve(options.hostname);
-  });
-}
-
-module.exports = getToken;
-
-// async function getToken(options, postData) {
-//   const req = https.request(options, async (res) => {
-//     console.log('statusCode:', res.statusCode);
-//     console.log('headers:', res.headers);
-
-//     const auth = res.on('data', (d) => {
-//       process.stdout.write(d);
-//       const auth = JSON.parse(d);
-//       return auth;
-//     });
-//   });
-//   req.on('error', (e) => {
-//     console.error(e);
-//   });
-
-//   req.write(postData);
-//   req.end();
-//   return auth;
-// }
-
-// const auth = getToken(options, postData);
-// module.exports = auth;
-
-/*
-const ClientPasswordStrategy = require('passport-oauth2-client-password').Strategy;
-
-
-passport.use(new ClientPasswordStrategy(
-  function(clientId, clientSecret, done) {
-  Clients.findOne({ clientId: clientId }, function (err, client) {
-    if (err) { return done(err); }
-    if (!client) { return done(null, false); }
-    if (client.clientSecret != clientSecret) { return done(null, false); }
-    return done(null, client);
-  });
-}
-));
-*/
