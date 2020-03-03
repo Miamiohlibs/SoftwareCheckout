bcfed63f707b4b8d0fac37761772bb2d281bd67b
Ken Irwin <irwinkr@miamioh.edu>
2020-03-03 09:41:27 -0500

    oop.js gets both tokens

--- oop.js
+++ oop.js
@@ -2,16 +2,30 @@
 const Query = require('./classes/Query');
 const LibCalApi = require('./classes/LibCalApi');
 const libCalConf = require('./config/libCal');
+const CampusApi = require('./classes/CampusApi');
+const campusConf = require('./config/campusIT');
+
+// comment this line out to suppress debug messages
+console.debug = ()=>{};
 
 (async () => { 
   const startTime = new Date().getTime();
 
+  // Get LibCal Token
   try {
     lcApi = new LibCalApi(libCalConf);
-    lcToken = await lcApi.getToken();
-    console.log('LibCal token:', lcToken)
+    const lcToken = await lcApi.getToken();
+    console.debug('LibCal token:', lcToken)
   } catch {
     console.error('Unable to get LibCal Token');
   }
 
+  // Get CampusToken
+  try {
+    campusApi = new CampusApi(campusConf);
+    const campusToken = await campusApi.getToken();
+    console.debug('Campus token:', campusToken);
+  } catch {
+    console.error('Unable to get Campus Token');
+  }
 })();
