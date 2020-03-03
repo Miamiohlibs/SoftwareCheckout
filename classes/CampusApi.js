5f319fa488dccd7350c6352452be8e58caa821d1
Ken Irwin <irwinkr@miamioh.edu>
2020-03-03 10:40:03 -0500

    added CampusApi::getCampusListNames + tests

--- classes/CampusApi.js
+++ classes/CampusApi.js
@@ -3,6 +3,7 @@ const Query=require('./Query');
 module.exports = class CampusApi {
   constructor (conf) {
     this.conf = conf;
+    this.getCampusListNames();
   }
 
   async getToken() {
@@ -10,6 +11,11 @@ module.exports = class CampusApi {
     let response = await api.execute();
     let tokenObj = JSON.parse(response);
     this.token = tokenObj.data.token;
-    return;
+    return this.token;
+  }
+
+  getCampusListNames() {
+    this.campusListNames = this.conf.software.map(item => { return item.shortName });
+    return this.campusListNames;
   }
 }
--- test/CampusApi.test.js
+++ test/CampusApi.test.js
@@ -11,12 +11,20 @@ describe('Campus IT API', () => {
     expect(typeof api).toBe('object');
     expect(api.conf).toHaveProperty('connectConfig');
     expect(api.conf).toHaveProperty('software');
+    expect(api).toHaveProperty('campusListNames'); // should this be mocked to just calls getCampusListNames ?
   });
 
   it('should be able to get a token', async () => {
-    await api.getToken();
+    const token = await api.getToken();
     expect(typeof api.token).toBe('string');
     expect(api.token.length).toBeGreaterThan(10);
-    // expect(token).toEqual(api.token);
+    expect(token).toEqual(api.token);
   });
+
+  it('should get the list of campus IT software items', () => {
+    let lists = api.getCampusListNames();
+    expect(lists).toBeInstanceOf(Array);
+    expect(lists).toContain('adobecc');
+    expect(lists).toEqual(api.campusListNames);
+  })
 });
