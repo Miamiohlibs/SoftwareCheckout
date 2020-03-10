386ff1df209a76575a2df912ca6bd45fcef40456
Ken Irwin <irwinkr@miamioh.edu>
2020-03-10 14:43:21 -0400

    added async package; oop.js formats libcal data ok

--- oop.js
+++ oop.js
@@ -4,6 +4,7 @@ const LibCalApi = require('./classes/LibCalApi');
 const libCalConf = require('./config/libCal');
 const CampusApi = require('./classes/CampusApi');
 const campusConf = require('./config/campusIT');
+const async = require('async');
 
 // comment this line out to suppress debug messages
 // console.debug = ()=>{};
@@ -40,10 +41,21 @@ const campusConf = require('./config/campusIT');
   // Get LibCal Lists
   try {
     let lcSoftware = await lcApi.getLibCalLists();
-    lcSoftware = lcApi.mapLibCal2CampusCodes(lcSoftware, campusConf.software)
+    lcSoftware = lcApi.mapLibCal2CampusCodes(lcSoftware, campusConf.software);
     console.debug(JSON.stringify(lcSoftware, null, 4));
+    // lcBookings = lcApi.getCurrentLibCalBookings(lcSoftware);
+    let lcUserList = {};
+
+    await async.eachOf(lcSoftware, async software => {
+      let lcBookings = lcApi.getCurrentLibCalBookings(software.bookings)
+      let emailBookings = lcApi.getEmailsFromBookings(lcBookings);
+      // console.log('Email bookings', emailBookings);
+      lcUserList[software.campusCode] = await campusApi.convertMultipleEmails(emailBookings);
+      // console.log('userlist:', software.campusCode, lcUserList[software.campusCode]);
+    });
+
+    console.log('LibCal bookings:', lcUserList);
   } catch (err) {
     console.error('Error getting LibCal lists:', err);
   }
-
 })();
