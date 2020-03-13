8f32b98ec2a0e59ad85a1b3f6be36f5e857affae
Ken Irwin <irwinkr@miamioh.edu>
2020-03-13 15:25:44 -0400

    AdobeUserMgmtApi::querySetup() tests ok

--- classes/AdobeUserMgmtApi.js
+++ classes/AdobeUserMgmtApi.js
@@ -17,4 +17,27 @@ module.exports = class AdobeUserMgmtApi {
     return tokenResponse.access_token;
     // this.accessToken = tokenResponse;
   }
+
+  // start with a basic set of options, add or overwrite with new options
+  querySetup(baseOpts, opts) {
+    this.currOpts = JSON.parse(JSON.stringify(this.queryConf[baseOpts].options)); //clone values, don't pass by reference
+
+    if (opts.hasOwnProperty('headers')) { 
+      var tmpHeaders = opts.headers;
+      delete opts.headers;
+    }
+    this.currOpts = Object.assign(this.currOpts, opts);
+    if (typeof tmpHeaders !== 'undefined') {
+     this.currOpts.headers = Object.assign(this.currOpts.headers, tmpHeaders); 
+    }
+
+
+    // for (const property in opts) {
+    //   // if(property === 'headers') {
+    //   //   //add to headers, don't overwrite them
+    //   //   this.currOpts.headers.assign(opts['headers']);
+    //   // }
+    //   this.currOpts = Object.assign(this.currOpts, opts[property]);
+    // }
+  }
 }
--- test/AdobeApi.test.js
+++ test/AdobeApi.test.js
@@ -19,8 +19,7 @@ describe('Initialization', () => {
   });
 })
 
-describe('getToken', () => {
-
+describe('Queries', () => {
   beforeEach(async () => {
     api = new AdobeUserMgmtApi(realConf);
     await api.getToken();
@@ -30,4 +29,26 @@ describe('getToken', () => {
     expect(api).toHaveProperty('accessToken');
     expect(typeof api.accessToken).toBe('string');
   });
+
+  it('should be able to add queryConfigs using querySetup() with extra headers', () => {
+    var genericOpts =  api.queryConf.generic.options; 
+    console.log('genericOpts', genericOpts)
+    const originalHeadersLength = Object.keys(genericOpts.headers).length; 
+    api.querySetup('generic', {fake: 'aardvark', bogus: 'pangolin', headers: { artificial: 'imaginary'}} );
+    expect(api.currOpts).toHaveProperty('fake','aardvark');
+    expect(api.currOpts).toHaveProperty('bogus','pangolin');
+    expect(api.currOpts.headers).toHaveProperty('artificial','imaginary');
+    expect(Object.keys(api.currOpts.headers).length).toEqual(originalHeadersLength + 1);
+  })
+
+  it('should be able to add queryConfigs using querySetup() with NO extra headers', () => {
+    var genericOpts =  api.queryConf.generic.options; 
+    // console.log('genericOpts', api.queryConf.generic.options);
+    const originalHeadersLength = Object.keys(genericOpts.headers).length; 
+    api.querySetup('generic', {fake: 'aardvark', bogus: 'pangolin'} );
+    expect(api.currOpts).toHaveProperty('fake','aardvark');
+    expect(api.currOpts).toHaveProperty('bogus','pangolin');
+    expect(api.currOpts.headers).not.toHaveProperty('artificial');
+    expect(Object.keys(api.currOpts.headers).length).toEqual(originalHeadersLength);
+  })
 });
